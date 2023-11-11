b28fdade126f40d5548508ea027cde7674454c4a
Artem <artem.xolod.90@gmail.com>
2023-11-12 00:40:15 +0300

    Настройка папок для корректной работы роутинга

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import {Button} from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+        <>
+            <p className="text-6xl text-green-500">Hello (Protected)</p>
+            <Button> asd </Button>
+        </>
+
+    )
+}
+
+export default DashboardPage;
